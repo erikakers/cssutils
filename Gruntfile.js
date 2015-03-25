06cea601875ba97efea3ece22cdd2f6ee292b292
Erik Akers <erikakers@gmail.com>
2015-03-24 21:44:51 -0500

    changing to use load-grunt-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,8 @@
-'use strict';
+/**
+ * Created by erikakers on 3/24/15.
+ */
 
 module.exports = function(grunt) {
-
-	require('time-grunt')(grunt);
-	require('jit-grunt')(grunt)
-	grunt.template.addDelimiters('custom', '{%', '%}');
-
-	function loadConfig(path) {
-		var glob = require('glob');
-		var object = {};
-		var key;
-
-		glob.sync('*', {cwd: path}).forEach(function(option) {
-			key = option.replace(/\.js$/,'');
-			object[key] = require(path + option);
-		});
-
-		return object;
-	}
-
-	var config = {
-		pkg: grunt.file.readJSON('package.json'),
-		bowerrc: grunt.file.readJSON('.bowerrc'),
-
-		banner: '/*!\n' +
-			' * <%= pkg.name %>-<%= pkg.version %>\n' +
-			' * <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-			' */\n\n',
-
-		config: {
-			src: 'src',
-			build: 'app',
-			dist: 'dist',
-			vendor: '<%= bowerrc.directory %>'
-		}
-	};
-
-	grunt.util._.extend(config, loadConfig('./grunt/options/'));
-	grunt.initConfig(config);
-	grunt.loadTasks('grunt');
-	grunt.loadNpmTasks('assemble');
+    "use strict";
+    require('load-grunt-config')(grunt);
 };
